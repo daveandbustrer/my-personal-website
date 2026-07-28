7016c5602e98f51719480c5bcbb45ee5f45c6f3b
daveandbustrer <praneelcatchu@gmail.com>
2026-07-28 15:58:45 -0400

    fixed navbar

--- navbar/navBar.js
+++ navbar/navBar.js
@@ -1,2 +1,5 @@
-
-fetch("navbar.html").then(res => res.text()).then(html => document.getElementById("navbar").innerHTML = html);
+window.onload = () => {
+    fetch("/navbar/navbar.html")
+        .then(res => res.text())
+        .then(html => document.getElementById("navbar").innerHTML = html);
+};
