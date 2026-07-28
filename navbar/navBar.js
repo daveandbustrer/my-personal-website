43f3b1528ed93da6bc8b959c7e4aaa0168b71a75
daveandbustrer <praneelcatchu@gmail.com>
2026-07-28 16:42:15 -0400

    attempt 5 for navbar fix

--- navbar/navBar.js
+++ navbar/navBar.js
@@ -1,4 +1,4 @@
 window.onload = () => {
-    fetch("navbar/navbar.html")|fetch("https://daveandbustrer.github.io/my-personal-website/navbar/navbar.html")
+    fetch("navbar/navbar.html")|fetch("/my-personal-website/navbar/navbar.html")
         .then(html => document.getElementById("navbar").innerHTML = html);
 };
