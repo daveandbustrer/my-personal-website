e4ea1e25c2b2e09ecd82eb40ff9ec7e9de9da1fa
daveandbustrer <praneelcatchu@gmail.com>
2026-08-06 22:09:03 -0400

    navbar works

--- navBar.js
+++ navBar.js
@@ -1,5 +1,17 @@
-fetch(`${base}/navbar/navbar.html`)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("navbar").innerHTML = html;
-    })
+var html = `<nav>
+  <ul>
+    <li class="Home">
+      <a href="index.html" class="left">Home</a>
+    </li>
+    <li class="aboutme">
+      <a href="aboutme.html" class="center">About Me</a>
+    </li>
+    <li class="projects">
+      <a href="projects.html" class="center">Projects</a>
+    </li>
+    <li>
+      <a href="devlogs.html" class="right">devlogs</a>
+    </li>
+  </ul>
+</nav>`
+document.getElementById("navbar").innerHTML = html
